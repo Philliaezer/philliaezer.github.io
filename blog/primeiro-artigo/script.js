d087a82f576e59a5f291fc9fee79bc5b31a0ab19
Anderson Duarte <reddragondggda@gmail.com>
2023-02-20 23:28:36 -0200

    include comment

--- blog/primeiro-artigo/script.js
+++ blog/primeiro-artigo/script.js
@@ -1,1 +1,16 @@
 // Não vai ter nada de interessante por enquanto 
+const patti = /oi/;
+
+/*
+
+url = "/path/to/file.md?nonce=" + Date.now().toString(); // added nonce to prevent caching, you can remove it if you want the files to cache
+$.ajax({
+  url: url,
+  type: "GET",
+  dataType: "text",
+  success: function (results) {
+    var html = self.converter.makeHtml(results);
+    // do something with the html here
+});
+
+*/
