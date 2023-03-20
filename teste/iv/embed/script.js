237317fcadd19496d47d89804f915e980e08529b
Anderson Duarte <reddragondggda@gmail.com>
2023-03-19 21:59:03 -0300

    ultracommit

--- teste/iv/embed/script.js
+++ teste/iv/embed/script.js
@@ -1,1 +1,6 @@
 // Iniciando Javascript
+
+(function() {
+    VK.Widgets.Post("vk_post_1_2442097", 1, 2442097, 'RT56JEzdcHrbdFASDlawi7_LB5aw');
+    VK.Widgets.Post("vk_post_1_2422169", 1, 2422169, 'IwGqYjYffblFYBEE9H3C8QQJTPL3');
+  }());
